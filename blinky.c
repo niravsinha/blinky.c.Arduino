a6973a06fef4c871093370d997bbad418e8bb436
Nirav Sinha <niravsinha@users.noreply.github.com>
2016-09-07 15:09:53 +0530

    Update blinky.c

--- blinky.c
+++ blinky.c
@@ -12,20 +12,8 @@ int main (void)
     for (;;) {
         // toggle PORTB pins
         PORTB ^= 0xFF;
-        // wait one second
+        // wait one hundred millisecond
         _delay_ms(100);
-        // toggle PORTB pins
-        PORTB ^= 0xFF; 
-        // wait one second
-        _delay_ms(50);     
-        // toggle PORTB pins
-        PORTB ^= 0xFF; 
-        // wait one second
-        _delay_ms(3);
-        // toggle PORTB pins
-        PORTB ^= 0xFF; 
-        // wait one second
-        _delay_ms(20);                            
     }
     return 0;
 }
